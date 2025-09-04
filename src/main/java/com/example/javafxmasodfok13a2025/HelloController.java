5a429d16ae413d5d94c63107c2311a51589c27dd
Zsolt Harmath <harmathzs@kkando.hu>
2025-09-04 13:02:47 +0200

    Create JavaFX project

---
+++ src/main/java/com/example/javafxmasodfok13a2025/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.javafxmasodfok13a2025;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.javafxmasodfok13a2025 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.javafxmasodfok13a2025 to javafx.fxml;
+    exports com.example.javafxmasodfok13a2025;
+}
