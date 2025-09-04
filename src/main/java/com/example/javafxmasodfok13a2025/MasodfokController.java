3f919b16a589a84707ea6bbb6c451e10bbc6fb2a
Zsolt Harmath <harmathzs@kkando.hu>
2025-09-04 14:01:36 +0200

    Solve if had 2 different solutions

--- src/main/java/com/example/javafxmasodfok13a2025/MasodfokController.java
+++ src/main/java/com/example/javafxmasodfok13a2025/MasodfokController.java
@@ -1,14 +1,60 @@
 package com.example.javafxmasodfok13a2025;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MasodfokController {
+    @FXML
+    public TextField textfield_A;
+    @FXML
+    public TextField textfield_B;
+    @FXML
+    public TextField textfield_C;
+    @FXML
+    public Button button_Solve;
+    @FXML
+    public ListView listview_Solution;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    protected void onSolveButtonClick() {
+        // TODO - implement method
+        //System.out.println("onSolveButtonClick start");
+
+        // get a, b, c
+        double a = Double.parseDouble(textfield_A.getText());
+        double b = Double.parseDouble(textfield_B.getText());
+        double c = Double.parseDouble(textfield_C.getText());
+
+        // calculate d
+        double d = b*b - 4.0*a*c;
+
+        // TODO - check count of solutions
+
+        // solve if had 2 different solutions
+        double x1 = ( -b + Math.sqrt(d) )/( 2.0*a );
+        double x2 = ( -b - Math.sqrt(d) )/( 2.0*a );
+
+        String line1 = "x1 = "+x1; // TODO - round
+        String line2 = "x2 = "+x2;
+
+        List<String> lines = new ArrayList<>();
+        lines.add(line1);
+        lines.add(line2);
+        ObservableList<String> linesFX = FXCollections.observableList(lines);
+        listview_Solution.setItems(linesFX);
     }
 }
